edafe1ec73cf8a4d9ec05ece06245f0f25880a55
Aditya Shrivastava <adityashrivastava2005@gmail.com>
2025-03-12 11:30:25 +0530

    Initial commit: Set up Spring Boot project and add .gitignore

---
+++ abhayini/src/main/java/com/abhayini/AbhayiniApplication.java
@@ -0,0 +1,13 @@
+package com.abhayini;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AbhayiniApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AbhayiniApplication.class, args);
+	}
+
+}
